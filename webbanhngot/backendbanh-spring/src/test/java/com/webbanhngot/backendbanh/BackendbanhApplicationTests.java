21c986f168333feb820a0bf8ff09849ac7868b09
dirus182 <146466664+dirus182@users.noreply.github.com>
2026-05-31 15:49:17 +0700

    Initialize Spring Boot backend project
    - Add Spring Boot Maven project for bakery backend.
    - Configure Spring Web, JDBC API, and PostgreSQL Driver dependencies.
    - Add PostgreSQL datasource configuration.
    - Add initial HelloController endpoint.
    - Verify Spring Boot server runs on port 8080.

---
+++ webbanhngot/backendbanh-spring/src/test/java/com/webbanhngot/backendbanh/BackendbanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbanhngot.backendbanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
